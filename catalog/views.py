bc82727253696c8c36256f16ae32cfc8529f4fd2
Acacio.coding <acacio.coding@gmail.com>
2022-01-05 16:59:12 -0300

    Managing visitors

--- catalog/views.py
+++ catalog/views.py
@@ -8,12 +8,15 @@ def index(request):
     num_instances = BookInstance.objects.all().count()
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
     num_authors = Author.objects.count()
+    num_visitors = request.session.get('num_visitors', 0)
+    request.session['num_visitors'] = num_visitors + 1
 
     context = {
       'num_books': num_books,
       'num_instances': num_instances,
       'num_instances_available': num_instances_available,
       'num_authors': num_authors, 
+      'num_visitors': num_visitors,
     }
 
     return render(request, 'index.html', context = context)
